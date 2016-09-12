2aafe3125c57b109bf04a8e0de237e8e1f089c3e
Akiraff2015 <akiraffmc@gmail.com>
2016-09-13 05:31:10 +0800

    New algorithm in generating random tiles.

--- js/Map.js
+++ js/Map.js
@@ -8,45 +8,54 @@
     var tileSize = 32;
     var imageNumTiles = 6;
 
-    var ground = [
+    var map = [
         [test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId()],
         [test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId()],
         [test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId(), test10.getId()],
         [grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId(),grass.getId()]
     ];
 
-    console.log();
-
-
     function drawMap(getMapRow) {
-        ground.push(getMapRow);
-        var rowTileCount = ground.length;
-        var colTileCount = ground[0].length;
+        map.push(getMapRow);
+        var rowTileCount = map.length;
+        var colTileCount = map[0].length;
 
         for (var r = 0; r < rowTileCount; r++) {
             for (var c = 0; c < colTileCount; c++) {
-                var tile = ground[r][c];
+                var tile = map[r][c];
                 var tileRow = (tile / imageNumTiles) | 0;
                 var tileCol = (tile % imageNumTiles) | 0;
                 ctx.drawImage(tilesetImage, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize);
             }
         }
     }
+    //Gives a probability of a chance, of a tile being placed.
+    function buildTile() {
+        var tileType = [borderRock, greenOre, blueRock];
+        var rand = Math.random();
+        if (tileType[1].getProbability() > rand) {
+            console.log(rand);
+            return tileType[1].getId();
+        }
 
-    //TODO: Generate 1 random row.
+        if (tileType[2].getProbability() > rand) {
+            console.log(rand);
+            return tileType[2].getId();
+        }
+
+        else {
+            return tileType[0].getId();
+        }
+    }
+
+
+    //TODO: Generate 1 random row. When key is pressed
     function generateRow() {
         var tempRowMap = [];
 
-        //Generate 28 new columns.
-        for (var i = 0; i < 28; i++) {
-            var rand = Math.round(Math.random()*5);
-            if (rand >= 1) {
-                tempRowMap.push(greenOre.getId());
-            }
-
-            else{
-                tempRowMap.push(borderRock.getId());
-            }
+        //Generate random tile for each column.
+        for (var i = 0; i < map[0].length; i++ ) {
+            tempRowMap.push(buildTile());
         }
         return tempRowMap;
     }
@@ -60,7 +69,9 @@
 
             //Press 's'
             if (e.keyCode == 83) {
-                //drawMap();
+                var imageData = ctx.getImageData(1, 0, ctx.canvas.width - 1, ctx.canvas.height);
+                ctx.putImageData(imageData, 0, 0);
+                ctx.clearRect(ctx.canvas.width-1, 0, 1, ctx.canvas.height);
                 console.log("s");
             }
 
